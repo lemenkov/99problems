5e62fd8216b1910fa4889eaf382761c4ed8d3b22
Peter Lemenkov <lemenkov@gmail.com>
2011-10-11 23:39:09 +0400

    Problem 3.01
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

---
+++ logic.erl
@@ -0,0 +1,37 @@
+% https://sites.google.com/site/prologsite/prolog-problems/3
+
+-module(logic).
+
+-compile([export_all]).
+
+% 3.01 (**) Truth tables for logical expressions.
+%	table(A,B,and(A,or(A,B))).
+problem1() ->
+	[io:format("~p ~p \t ~p~n", [X,Y,pred_and(X,pred_or(X,Y))]) || X <- [false, true], Y <- [false, true]].
+
+%%
+%% Private fun
+%%
+
+pred_not(false) -> true;
+pred_not(true) -> false.
+
+pred_and(true, true) -> true;
+pred_and(_, _) -> false.
+
+pred_or(true, _) -> true;
+pred_or(_, true) -> true;
+pred_or(_, _) -> false.
+
+pred_nand(A,B) -> pred_not(pred_and(A,B)).
+
+pred_nor(A,B) -> pred_not(pred_or(A,B)).
+
+pred_xor(true, true) -> false;
+pred_xor(false, false) -> false;
+pred_xor(_, _) -> true.
+
+pred_impl(true, false) -> false;
+pred_impl(_, _) -> true.
+
+pred_equ(A, B) -> pred_not(pred_xor(A,B)).
